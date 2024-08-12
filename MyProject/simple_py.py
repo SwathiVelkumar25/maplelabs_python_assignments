126283766638bbe348add46aeca3807154ee024a
swathi <swathivelkumar00@gmail.com>
2024-08-12 09:20:12 +0530

    successful

---
+++ MyProject/simple_py.py
@@ -0,0 +1,12 @@
+def count_char_frequencies(s):
+    freq_dict = {}
+    for char in s:
+        if char in freq_dict:
+            freq_dict[char] += 1
+        else:
+            freq_dict[char] = 1
+    return freq_dict
+
+# Example usage:
+s = "hello world"
+print(count_char_frequencies(s))
